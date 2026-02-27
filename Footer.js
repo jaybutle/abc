d75c5c373b863b5de77d490f9290ada320db21fa
Jay Mangesh Butle <142536378+jaybutle@users.noreply.github.com>
2026-02-27 23:53:00 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='flex flex-col center mt-10 mb-5'>
+            <a href='https://github.com/jaybutle'>
+                 By Jay Butle .
+
+            </a>
+        </footer>
+      
+    </div>
+  )
+}
+
+export default Footer
