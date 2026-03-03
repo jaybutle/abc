5b98f05d29bd4ab1502dc15c2558ab9d81d4703f
Jay Mangesh Butle <142536378+jaybutle@users.noreply.github.com>
2026-03-03 23:37:34 +0530

    Update footer text from 'By' to 'Made By'

--- Footer.js
+++ Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
         <footer className='flex flex-col center mt-10 mb-5'>
             <a href='https://github.com/jaybutle'>
-                 By Jay Butle .
+                Made By Jay Butle .
 
             </a>
         </footer>
